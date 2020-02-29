76cc998166da30493de9a6a50bfc8f7cb3876551
TZealot <55492310+TZealot@users.noreply.github.com>
2020-02-29 16:16:16 +0200

    12345

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Terms from "./Terms";
 
 
 class Main extends React.Component {
 
     render() {
         return (
-            <div id={'test'} className={'col-md-12'}>
+            <div className={'col-md-12'}>
+                <Terms/>
             </div>
         )
     }

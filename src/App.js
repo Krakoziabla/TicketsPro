7fc53372e7c067eb2a8699a9bf089da74b358bda
Андрей <vladon0wola@gmail.com>
2020-03-01 09:40:44 +0200

    87

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import CalendarMain from "./components/Calendar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-
 function App() {
     return (
         <div className="App">
